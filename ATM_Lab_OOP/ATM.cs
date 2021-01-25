f83271cd8b1e50f3bf1678b95ac5af9f13be0cf5
duncaannn <drbigfupa@yahoo.com>
2021-01-25 16:49:23 -0500

    Add project files.

---
+++ ATM_Lab_OOP/ATM.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ATM_Lab_OOP
+{
+    class ATM
+    {
+        private Account currentAccount;
+        private List<Account> accounts;
+
+        public Account CurrentAccount
+        {
+            get { return currentAccount; }
+            set { currentAccount = value; }
+        }
+        public List<Account> Accounts
+        {
+            get { return accounts; }
+            set { accounts = value; }
+        }
+
+        public ATM()
+        {
+            Accounts = new List<Account>();
+            CurrentAccount = null;
+        }
+        
+        public void Register(string name, string password)
+        {
+            Account account = new Account(name, password);
+            //Account account= new Account();
+            //account.Name = name;
+            //account.Password = password;
+
+            Accounts.Add(account);
+            Console.WriteLine("Successfully Registered.");
+        }
+
+        public void Login (string username, string password)
+        {
+            if (CurrentAccount==null)
+            {
+                for (int i = 0; i <Accounts.Count; i++)
+                {
+                    if(Accounts[i].Name == username && Accounts[i].Password == password)
+                    {
+                        CurrentAccount = Accounts[i];
+                        Console.WriteLine("Successfully Logged in...");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error. User still logged in.");
+            }
+
+        }
+        public void Logout()
+        {
+            CurrentAccount = null;
+        }
+
+        public void CheckBalance()
+        {
+            Console.WriteLine($"Your current balance is {CurrentAccount.Balance:C2}");
+        }
+        public void Deposit (int amount)
+        {
+            CurrentAccount.Balance += amount;       
+        }
+public void Withdraw(int amount)
+    {
+        if(CurrentAccount.Balance >= amount)
+        {
+                CurrentAccount.Balance -= amount;
+        }
+        else
+            {
+                Console.WriteLine("Insufficient Funds!");
+            }
+    }
+    }
+}
---
+++ ATM_Lab_OOP/Account.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ATM_Lab_OOP
+{
+    class Account
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public int Balance { get; set; }
+
+        public Account() { }
+        public Account (string Name, string Password, int Balance)
+        {
+            this.Name = Name;
+            this.Password = Password;
+            this.Balance = Balance;
+
+        }
+
+        public Account(string name, string password)
+        {
+            Name = name;
+            Password = password;
+        }
+    }
+       
+}
